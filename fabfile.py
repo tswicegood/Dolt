eb51029ffabcc447c72a78ccb120ed0f10ce5209
Travis Swicegood <development@domain51.com>
2010-01-06 09:53:16 -0600

    add setup.py and a few TODOs to the fabfile

---
+++ fabfile.py
@@ -0,0 +1,2 @@
+# TODO: automate building of sdist
+# TODO: automate uploading of sdist to github.com
---
+++ setup.py
@@ -0,0 +1,47 @@
+from distutils.core import setup
+import os
+
+# Stolen from django-registration
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir:
+    os.chdir(root_dir)
+
+for dirpath, dirnames, filenames in os.walk('registration'):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        pkg = dirpath.replace(os.path.sep, '.')
+        if os.path.altsep:
+            pkg = pkg.replace(os.path.altsep, '.')
+        packages.append(pkg)
+    elif filenames:
+        prefix = dirpath[13:] # Strip "registration/" or "registration\"
+        for f in filenames:
+            data_files.append(os.path.join(prefix, f))
+
+
+setup(
+    name='Dolt',
+    version='0.1', # TODO: move this into Dolt.get_version()
+    description='A dumb little wrapper around RESTful interfaces',
+    author='Travis Swicegood',
+    author_email='development@domain51.com',
+    url='http://github.com/tswicegood/Dolt/',
+    packages=packages,
+    package_data={'dolt': data_files},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Common Development and Distribution License (CDDL)',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)'
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+)
