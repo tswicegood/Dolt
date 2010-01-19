a31dd2f2d2645db679e8a338235b6c7751354508
Chris Dickinson <christopher.s.dickinson@gmail.com>
2010-01-19 10:42:54 -0600

    just a wee bit more twiddling.

--- dolt/apis/bitly.py
+++ dolt/apis/bitly.py
@@ -6,12 +6,12 @@ class Bitly(Dolt):
         self._defaults = {
             'login':None,
             'apiKey':None,
-            'apiVersion':'2.0.1',
+            'version':'2.0.1',
             'format':'json',
         }
         self._defaults.update(kwargs)
         self._api_url = 'http://api.bit.ly'
-        self._url_template = '%(domain)s/%(generated_url)s.json'
+        self._url_template = '%(domain)s/%(generated_url)s'
 
     def __call__(self, *args, **kwargs):
         updated_kwargs = kwargs
