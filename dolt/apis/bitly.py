69d76e1bbaa7e78ac1790f35eecd350f2d89b4d6
Chris Dickinson <christopher.s.dickinson@gmail.com>
2010-01-19 10:12:09 -0600

    initials of bitly dolt api

---
+++ dolt/apis/bitly.py
@@ -0,0 +1,18 @@
+from dolt import Dolt
+
+class Bitly(Dolt):
+    def __init__(self, *args, **kwargs):
+        super(Bitly, self).__init__(*args, **kwargs)
+        self._defaults = {
+            'username':None,
+            'api_key':None,
+            'api_version':'2.0.1',
+            'format':'json',
+        }
+        self._defaults.update(kwargs)
+        self._api_url = 'http://api.bit.ly'
+        self._url_template = '%(domain)s/%(generated_url)s.json'
+
+    def __call__(self, *args, **kwargs):
+        updated_kwargs = kwargs.update(self._defaults)
+        super(Bitly, self).__call__(*args, **updated_kwargs)
