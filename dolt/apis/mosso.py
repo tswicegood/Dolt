e8b7c15e14f4e01ee9471fcd355779b2cf782dcb
Travis Swicegood <development@domain51.com>
2010-02-03 10:00:38 -0600

    finish fleshing out the Mosso API

--- dolt/apis/mosso.py
+++ dolt/apis/mosso.py
@@ -1,5 +1,7 @@
 from dolt import Dolt
+import datetime
 import httplib2
+import simplejson
 
 MOSSO_AUTH_URL = "https://auth.api.rackspacecloud.com"
 
@@ -22,7 +24,7 @@ class MossoHttp(object):
     def initialize_auth_token(self):
         if self.auth_token:
             return
-        url = "%s/v%s" % (MOSSO_AUTH_URL, self.version)
+        url = "%s/v%s?cache=%s" % (MOSSO_AUTH_URL, self.version, datetime.datetime.now().microsecond)
         response, _body = self.http.request(url, headers = {
             'X-Auth-User': self.username,
             'X-Auth-Key': self.api_key,
@@ -35,9 +37,42 @@ class MossoHttp(object):
 class MossoServers(Dolt):
     def __init__(self, username, api_key, **kwargs):
         http = MossoHttp(username=username, api_key=api_key)
+        self._last_url = None
         super(MossoServers, self).__init__(http=http)
 
     def get_url(self):
         self._http.initialize_auth_token()
         self._api_url = self._http.server_url
-        return super(MossoServers, self).get_url()
+        self._last_url = super(MossoServers, self).get_url()
+        return self._last_url
+
+    def _generate_body(self):
+        def should_contain_values():
+            if self._attribute_stack[-1] == 'servers':
+                return True
+            elif self._attribute_stack[0] == 'servers' and len(self._attribute_stack) == 2:
+                try:
+                    int(self._attribute_stack[-1])
+                    return True
+                except ValueError:
+                    pass
+            return False
+        if self._method in ('POST', 'PUT'):
+            if should_contain_values():
+                values = {'server': self._params}
+            return simplejson.dumps(values)
+
+    def _handle_response(self, response, data):
+        if self._method in ('DELETE', 'PUT'):
+            return {
+                "202": "Completed",
+                "204": "Successful",
+                "400": "Bad Request",
+                "401": "Unauthorized",
+                "404": "Item Not Found",
+                "409": "Build In-Progress",
+                "413": "Over Limit",
+                "500": "Cloud Servers Fault",
+                "503": "Service Unavailable",
+            }[response['status']]
+        return super(MossoServers, self)._handle_response(response, data)
