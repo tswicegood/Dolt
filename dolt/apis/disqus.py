e0fced4eb60f4b34ad5b3545a17a05fc83e1fe6c
Travis Swicegood <development@domain51.com>
2010-07-26 21:35:09 -0500

    add Disqus object for interacting with Disqus API

---
+++ dolt/apis/disqus.py
@@ -0,0 +1,9 @@
+from dolt import Dolt
+
+class Disqus(Dolt):
+    def __init__(self, user_api_key, version="1.1", *args, **kwargs):
+        super(Disqus, self).__init__(*args, **kwargs)
+        self._api_url = "http://disqus.com"
+        self._url_template = "%(domain)s/api/%(generated_url)s/?user_api_key=" + user_api_key + "&version=" + version
+
+
