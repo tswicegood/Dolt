6f34b2a6e61a2f80e86bf068e06ab98668c1d8c8
Travis Swicegood <development@domain51.com>
2010-11-20 14:15:32 -0600

    add release fab script

--- fabfile.py
+++ fabfile.py
@@ -1,2 +1,7 @@
 # TODO: automate building of sdist
 # TODO: automate uploading of sdist to github.com
+from fabric.api import local
+
+def release():
+    local('python setup.py sdist upload', capture=False)
+
